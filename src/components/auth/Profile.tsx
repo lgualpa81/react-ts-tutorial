46b4b410832c47bfdda4989565b900586ab74b3c
Leonardo Gualpa <lgualpa81@gmail.com>
2022-08-07 16:49:40 -0500

    component prop

---
+++ src/components/auth/Profile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+export type ProfileProps = {
+  name: string;
+};
+export const Profile = ({ name }: ProfileProps) => {
+  return <div>Private Profile Component. Name is {name}</div>;
+};
