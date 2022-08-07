edc87829e890b455c7202b240fb01ac187c3015b
Leonardo Gualpa <lgualpa81@gmail.com>
2022-08-07 14:18:53 -0500

    Basic-advanced props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Status } from './components/Status';
+import { Greet } from './components/Greet';
+// import { Person } from './components/Person';
+// import { PersonList } from './components/PersonList';
+// import { MuiButton } from './components/MuiButton';
+// import { MuiTypography } from './components/MuiTypography';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+  const nameList = [
+    {first: 'Bruce', last:'Wayne'},
+    {first: 'Peter', last:'Parker'},
+    {first: 'Clark', last:'Kent'},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet name='Human' messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} /> */}
+      <Status status='error'/>
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+      <Greet name='Human' isLoggedIn={false}/>
     </div>
   );
 }
-
+//react material ui
+//https://www.youtube.com/watch?v=aOZv5mXMxWI&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO&index=6
+//react typescript
+//https://www.youtube.com/watch?v=Kt4PQlcLHco
 export default App;
